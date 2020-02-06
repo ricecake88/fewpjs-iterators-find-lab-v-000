50f71d77a0f421e2652d5aa4eb6263228d356186
Grace Shih <vocalise@gmail.com>
2020-02-06 06:15:14 +0000

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,13 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(array_of_records) {
+ let record = array_of_records.find(game => game.result === "W" ? game.year : undefined)
+ if (record === undefined) {
+   return undefined
+ } else {
+   return record.year;
+ }
+  
+}
