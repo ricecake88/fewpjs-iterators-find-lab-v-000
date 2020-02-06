3de2e3d7e047f7adf608fda3d9a984ce8cea422f
Grace Shih <vocalise@gmail.com>
2020-02-06 06:11:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ function testFunc() {
 }
 
 function superbowlWin(array_of_records) {
-  array_of_records.find(game => game.result === "W" ? return game.year : return undefined)
+  array_of_records.find(game => game.result === "W" ? game.year : undefined)
   
 }
